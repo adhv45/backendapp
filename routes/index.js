534be330633f75022d87cffc3f22b4994b343f23
adhv45 <chhipaanil98@gmail.com>
2017-06-24 18:20:12 +0530

    new

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-var URL="mongodb://adhv_45:Anil%4097832@ds131492.mlab.com:31492/adhv45"
+var URL="mongodb://adhv_45:Anil%4097832@ds131492.mlab.com:31492/adhv45";
 
-const db= require('monk')(URL)
+const db= require('monk')(URL);
 
-const docs=db.get('anilc')
+const docs=db.get('anilc');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   //res.render('index', { title: 'Express' });
 
   docs.find({}, function (err,docs) {
-      if(err) console.log(err)
+      if(err) console.log(err);
       else {
           res.json(docs[0]);
       }
@@ -22,7 +22,7 @@ router.get('/', function(req, res, next) {
 router.get('/Wel', function(req, res, next) {
     //res.send('what is the problem');
     docs.insert({"name":"Amit","class":"M.tech"},function( err, docs) {
-        if(err) console.log(err)
+        if(err) console.log(err);
         else{
             res.send("successfulll!!");
         }
@@ -31,7 +31,7 @@ router.get('/Wel', function(req, res, next) {
 });
 router.get('/come', function(req, res, next) {
     docs.update({"id":"78uyui","name":"anil"},{$push:{"group":{"name":"user3"}}},function(err,docs) {
-        if (err) console.log(err)
+        if (err) console.log(err);
         else {
             res.send("successfull!");
         }
@@ -39,9 +39,9 @@ router.get('/come', function(req, res, next) {
 });
 router.get('/wec',function(req,res,next){
     docs.find({"name":"anil"},function(err,docs) {
-        if (err) console.log(err)
+        if (err) console.log(err);
         else {
-            res.json(docs);
+            res.json(docs[0]);
         }
     })
 });
