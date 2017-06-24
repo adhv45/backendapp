fe5b89e69bb7d827592914a6b1f3269f28f5c1d1
adhv45 <chhipaanil98@gmail.com>
2017-06-24 17:44:23 +0530

    app.ts

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var cors = require('cors');
+var methodOverride= require('method-override');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +23,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride());
+app.use(cors());
 
 app.use('/', index);
 app.use('/users', users);
+app.use(function (req,res,next) {
+  res.header("Access-Control-Allow-Origin","*");
+  res.header('Access-Control-Allow-Methods','DELETE,PUT');
+  res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+  next();
+
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
